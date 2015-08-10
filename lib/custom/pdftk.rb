455d31915d4ee3b6358e51f88d881666563ab560
kwaters12 <kellywaters@gmail.com>
2015-08-09 23:29:20 -0700

    Attempt fix again8

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -9,7 +9,7 @@ class Pdftk < BaseCustom
   end
 
   def source_url
-    "https://s3-us-west-2.amazonaws.com/noaapp/pdftk.pkg.zip"
+    "https://s3-us-west-2.amazonaws.com/noaapp/pdftk.tar.gz"
   end
 
   def used?

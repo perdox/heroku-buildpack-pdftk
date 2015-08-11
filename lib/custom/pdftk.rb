c46f5e1a31c99638904a73c427227687493faa36
kwaters12 <kellywaters@gmail.com>
2015-08-10 21:31:01 -0700

    Debugging

--- lib/custom/pdftk.rb
+++ lib/custom/pdftk.rb
@@ -20,6 +20,7 @@ class Pdftk < BaseCustom
     write_stdout "compiling #{name}"
     #download the source and extract
     %x{ mkdir -p #{path} && curl --silent #{source_url} -o - | tar -xz -C #{path} -f - } 
+    write_stdout "Files here: #{path}"
     write_stdout "complete compiling #{name}"
   end
 
